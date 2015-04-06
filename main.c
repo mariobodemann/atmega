70ef3a76588e2722cb4f6a4bfef69f11d1bbda13
Mario Bodemann <mario.bodemann@gmail.com>
2015-04-06 11:21:42 +0200

    Removing off by one (first led was not lighting up)

--- main.c
+++ main.c
@@ -7,9 +7,11 @@ int main (void)
 {
    DDRB = 0xFF; //set up pin 0 on port B
    int value = 1;
+   
    while (1) //loop forever
    {
-        PORTB = value <<= 1;
+        PORTB = value;
+        value <<= 1;
 	value = value % 0xFF;
 	_delay_ms(100);
    }
